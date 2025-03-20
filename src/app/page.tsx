a2f9c55e423560cbc076e877500f586bc996e7ff
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2025-03-20 19:38:29 -0300

    feat: add apresentetio component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Apresentation from "@/components/apresentatios";
 import Header from "@/components/header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Apresentation/>
     </>
   );
 };
---
+++ src/components/apresentatios.tsx
@@ -0,0 +1,12 @@
+const Apresentation = () => {
+  return (
+    <div className="justify-center flex flex-col items-center mt-24 py-4 px-4">
+      <div>
+        <h2 className="items-center text-center">Detetive particular</h2>
+        <p className="text-zinc-500 text-center mt-4">Porque a confiança é essencial. Vamos trazer a verdade à tona</p>
+      </div>
+    </div>
+  );
+};
+
+export default Apresentation;
